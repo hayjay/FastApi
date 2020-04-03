56902006ae8b4d5b08dd297c1bf38959de4ce679
Nurudeen Ajayi <ydeveloper15@gmail.com>
2020-04-03 22:53:16 +0100

    Use of Nested Models in Python FastApi

--- source/FormValidation.py
+++ source/FormValidation.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI, Query, Path, Body
 from pydantic import BaseModel, Field
-from typing import List, Set
+from typing import List, Set, Dict
 
 app = FastAPI(debug=True)
 
@@ -15,12 +15,24 @@ class Item(BaseModel):
     price : float = Field(..., gt=0, le=100, description="The price must be greater than zero and must be lesser than or equal 100")
     tax : float = None
     tags: List[str] = []
-    image : Image = None
+    images : List[Image] = None #a list of images should be passed in
     # tags : Set[str] = [] Set removes duplicate from a list of items
 
 class User(BaseModel):
     username : str
     full_name : str = None
+
+class Offer(BaseModel):
+    name : str
+    description : str = None
+    price : float
+    items: List[Item]
+
+@app.put("/get-ofer/{offer_id}")
+async def get_offer(item : int, offer : Offer):
+    results = {"item" : item, "offer" : offer}
+    return results
+
 
 @app.put("/items/{item_id}")
 async def update_item(*, item_id: int, item : Item = Body(..., embed=True, example = {
@@ -29,13 +41,26 @@ async def update_item(*, item_id: int, item : Item = Body(..., embed=True, examp
     "price" : 34.6,
     "tax" : 0.88,
     "tags" : ["me", "kola"],
-    "image" : {
-        "url" : "https://www.imageurl.com",
-        "name" : "image name"
-    }
+    "images" : [
+        {
+            "url" : "https://www.imageurl.com",
+            "name" : "Ajayi Image"
+        },
+        {
+            "url" : "https://www.cloudinaryimageurl.com",
+            "name" : "Nurudeen Image"
+        },
+    ]
 })):
     results = {"item_id" : item_id, "item" : item}
     return results
+
+@app.post("/create-post")
+# key value pair key must be int in quotes while float could be int or decimal points
+async def create_post(post : Dict[int, float]):
+    return post
+
+
 
 if __name__ == "__main__" :
     uvicorn.run(app, host = "127.0.0.1", port=8090)
